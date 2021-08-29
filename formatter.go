98e9cce0b45566206f21c6f268e1a6047f4914ce
tihardc <88244327+tihardc@users.noreply.github.com>
2021-08-29 08:17:46 -0700

    Add files via upload

--- formatter.go
+++ formatter.go
@@ -6,13 +6,39 @@ import (
 )
 
 const (
-	reset = "\033[0m"
-	red   = "\033[31m"
+	reset  = "\033[0m"
+	red    = "\033[31m"
+	yellow = "\033[33m"
+	green  = "\033[32m"
+	blue   = "\033[34m"
 )
 
 type (
 	formatter func(res []string, line string) string
 )
+
+// This assigns an integer value to a string in such a way that strings that are
+// the same modulo case get the same value. It's a bit of a kludge but meh.
+
+func foldedIndex(s string) int {
+	s = strings.ToLower(s)
+	i := 0
+	for _, r := range s {
+		i = i + int(r)
+	}
+	return i
+}
+
+func multicolorFormat(hits []string, line string) string {
+
+	rainbow := []string{red, yellow, green, blue}
+
+	for _, hit := range hits {
+		hilight := rainbow[foldedIndex(hit)%len(rainbow)]
+		line = strings.ReplaceAll(line, hit, fmt.Sprintf("%s%s%s", hilight, hit, reset))
+	}
+	return fmt.Sprintf("%s\n", line)
+}
 
 func colorFormat(hits []string, line string) string {
 	for _, hit := range hits {
--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"log"
-	"io"
 	"bufio"
 	"fmt"
+	"io"
+	"log"
 	"os"
 )
-
 
 func checkString(line string, keyWord string, checkFunc checkFunc, formatter formatter) (string, error) {
 	res, err := checkFunc(keyWord, line)
@@ -20,33 +19,36 @@ func checkString(line string, keyWord string, checkFunc checkFunc, formatter for
 	return formatter(res, line), nil
 }
 
-
 func main() {
-	
+
 	var criterion checkFunc
-	
+	var format formatter
+
 	config := mustGetConfig()
 
 	file, err := os.Open(config.filePath)
 	if err != nil {
-			panic(err)
+		panic(err)
 	}
 
 	defer file.Close()
 
 	reader := bufio.NewReader(file)
 	for {
-			line, _, err := reader.ReadLine()
+		line, _, err := reader.ReadLine()
 
-			if err == io.EOF {
-					break
-			}
-		switch config.option {
-		case "n", "N": criterion = containsCheck
-		case "y", "Y": criterion = containsCaseless
-		case "r", "R": criterion = containsRegEx
+		if err == io.EOF {
+			break
 		}
-		detected, err := checkString(string(line), config.keyString, criterion, colorFormat)
+		switch config.option {
+		case "n", "N":
+			criterion, format = containsCheck, colorFormat
+		case "y", "Y":
+			criterion, format = containsCaseless, colorFormat
+		case "r", "R":
+			criterion, format = containsRegEx, multicolorFormat
+		}
+		detected, err := checkString(string(line), config.keyString, criterion, format)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -54,6 +56,5 @@ func main() {
 		fmt.Print(detected)
 
 	}
-
 
 }
